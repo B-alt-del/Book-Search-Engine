5d02f6e5856a8abf30652ac611f96637996c3790
bradschillschool@gmail.com <bradschillschool@gmail.com>
2022-08-30 04:10:35 -0500

    fixed query bug

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<SearchBooks />} />
           <Route path='/saved' element={<SavedBooks />} />
-          <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>}/>
+          {/* <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>}/> */}
         </Routes>
       </div>
   );
